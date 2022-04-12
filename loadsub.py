86b787e91025265c7c1e286815c625f5901b5175
LowHigh <71192394+TheLowHigh@users.noreply.github.com>
2022-04-12 12:03:21 +0200

    Update loadsub.py

--- loadsub.py
+++ loadsub.py
@@ -24,7 +24,7 @@ async def on_ready():
     ██████╔╝╚██████╔╝   ██║   
     ╚═════╝  ╚═════╝    ╚═╝   
                           
-            By 2$py#5340
+            By LowHigh
 
     """)
     print(f"Connecté à : {bot.user.name}")
